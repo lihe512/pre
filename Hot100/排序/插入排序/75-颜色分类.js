3c238e73dc7b3b3b38ff77fb5fb30a88d35c556f
li-youzhi <1849469978@qq.com>
2025-12-30 10:15:22 +0800

    75-颜色分类

---
+++ Hot100/排序/插入排序/75-颜色分类.js
@@ -0,0 +1,48 @@
+/**
+ * 给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地 对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+必须在不使用库内置的 sort 函数的情况下解决这个问题。
+
+ 
+
+示例 1：
+
+输入：nums = [2,0,2,1,1,0]
+输出：[0,0,1,1,2,2]
+示例 2：
+
+输入：nums = [2,0,1]
+输出：[0,1,2]
+ 
+
+提示：
+
+n == nums.length
+1 <= n <= 300
+nums[i] 为 0、1 或 2
+ 
+
+进阶：
+
+你能想出一个仅使用常数空间的一趟扫描算法吗？
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    const n = nums.length;
+    for(let i= 1;i<n; i++){
+        let current = nums[i];
+        let j = i-1;//手里已排序的牌的最右边一张的下标
+        while(j>=0&&nums[j]>current){
+            nums[j+1] = nums[j];
+            j--;
+        }
+        nums[j+1] = current;
+    }
+};
